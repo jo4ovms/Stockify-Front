eff40066b617be9dec59ba56eb988fb36ac987e2
jo4ovms <joaovictormacields@gmail.com>
2024-10-24 11:08:18 -0300

    feat: better loading on app

--- stockifyweb2/src/main.jsx
+++ stockifyweb2/src/main.jsx
@@ -3,10 +3,23 @@ import React, { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense
+      fallback={
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "300px",
+          }}
+        >
+          <CircularProgress />
+        </div>
+      }
+    >
       <BrowserRouter>
         <App />
       </BrowserRouter>
